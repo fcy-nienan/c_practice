6f8d6bdfcc30afc914549d13495bff5df82b80e6
nienan <nienan@atyun.net>
2021-07-27 09:25:29 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 #include "sort.cpp";
 
 int main() {
+    int y=100;
+    int x[10];
+    for (int i = 0; i < 10; ++i) {
+        cout<<&x[i]<<endl;
+    }
+    int a=1111;
+    int b;
+    int c;
+    cout<<&y<<endl;
+    cout<<&a<<endl;
+    cout<<&b<<endl;
+    cout<<&c<<endl;
+    cout<<x+1<<endl;
+    cout<<*(x-3)<<endl;
 //    sort_test::test();
-    object_test::test();
+//    object_test::test();
 }
 
 
